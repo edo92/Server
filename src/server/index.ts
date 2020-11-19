6e7a9852be5b97dfd7c1d9df453f9014db6df898
Eduard Jacobs <edoter92@gmail.com>
2020-11-18 23:51:11 -0800

    test

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { connectMongo } from '../db'
+// import { connectMongo } from '../db'
 
 import * as bodyParser from 'body-parser';
 import router from '../routes';
 
 class Server {
     public app = express.application;
-    private port = 3000;
+    private port = 80;
 
     constructor() {
         this.app = express();
@@ -14,7 +14,7 @@ class Server {
     }
 
     public start(): void {
-        connectMongo();
+        // connectMongo();
         this.app.listen(this.port, (): void => {
             console.log('Server is listening');
         })
